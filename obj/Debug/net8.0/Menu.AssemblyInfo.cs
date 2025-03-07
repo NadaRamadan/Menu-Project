09b0348d8798d927b7b5e4b6dfc164ed45c2c145
NadaRamadan <nada7.ra@gmail.com>
2025-03-07 12:33:26 +0200

    Initial commit

--- obj/Debug/net8.0/Menu.AssemblyInfo.cs
+++ obj/Debug/net8.0/Menu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97300f4bcb7c4399ab0f0fcbaa919056d6f6df13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
